d65b166ef9b125c52d2147edd2c80abc472b4ae2
pen <pen@stacksoft.ru>
2020-06-07 16:39:33 +0300

    texbox и кнопка

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
+//
+//main() => runApp(Steria());
 
 void main() {
-  runApp(Steria());
+  runApp(SteriaApp());
 }
 
-class Steria extends StatelessWidget {
+class SteriaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Steria",
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.amber,
+        textTheme: TextTheme(title: TextStyle(color: Colors.red)),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-        home: SearchPage()
+        home: HomePage()
     );
   }
 
 }
+
+//class HomePage extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//   return Container(
+//     child: TextField(
+//         obscureText: false,
+//         decoration: InputDecoration(
+//           border: OutlineInputBorder(),
+//           labelText: "Введите улице и номер дома",
+//         )
+//     ),
+//   );
+//  }
+//
+//}
 
 class SearchPage extends StatefulWidget{
   @override
@@ -32,93 +51,148 @@ class StartPageState extends State<SearchPage>{
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text('¯\_(ツ)_/¯')
-//          TextFormField(
-//            validator: (value) {
-//              if (value.isEmpty) {
-//                return 'Введите название улицы и номер дома';
-//              }
-//            },
-//          ),
-//          Padding(
-//            padding: const EdgeInsets.symmetric(vertical: 16.0),
-//          )
-        ],
-      ),
-    );
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          elevation: 2.0,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(Icons.search),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: "Find our product"),
+                  ),
+                ),
+                Icon(Icons.menu),
+              ],
+            ),
+          ),
+        ),
+      )
+      );
+////          TextFormField(
+////            validator: (value) {
+////              if (value.isEmpty) {
+////                return 'Введите название улицы и номер дома';
+////              }
+////            },
+////          ),
+////          Padding(
+////            padding: const EdgeInsets.symmetric(vertical: 16.0),
+////          )
+//        ],
+//      ),
+//    );
   }
 
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Steria',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+class HomePage extends StatelessWidget{
+  Size deviceSize;
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  HomePage({Key key, this.onPressed}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Steria"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+  Widget searchBox() => Padding(
+    padding: const EdgeInsets.all(30.0),
+    child: Card(
+      elevation: 2.0,
+      child: Padding(
+        padding: const EdgeInsets.all(6.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Icon(Icons.search),
+            SizedBox(
+              width: 10.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Find our product"
+                ),
+              ),
             ),
+//             Icon(Icons.menu),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+    ),
+  );
+
+  final GestureTapCallback onPressed;
+
+  Widget searchButton() =>Padding(
+      padding: const EdgeInsets.all(30.0),
+    child: Material(
+      elevation: 10.0,
+      color: Colors.transparent,
+      shape: const StadiumBorder(),
+      child: InkWell(
+        onTap: onPressed,
+        splashColor: Colors.yellow,
+        child: Ink(
+            height: 50.0,
+            width: 150.0,
+            decoration: ShapeDecoration(
+                shape: const StadiumBorder(),
+                gradient: LinearGradient(
+                  colors: <Color>[Colors.red, Colors.blue],
+                )
+            ),
+            child: Center(
+                child: Text(
+                  'Найти',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 20.0),
+                )
+            )
+        )
+        )
+      )
+    );
+
+  Widget combo() => Padding(
+    padding: const EdgeInsets.all(6.0),
+    child: Column(
+      children: <Widget>[
+        searchBox(),
+        searchButton(),
+      ]
+    )
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    deviceSize = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Stack(
+            fit: StackFit.passthrough,
+            children: <Widget>[
+              combo(),
+            ]
+        )
     );
   }
 }
+
+//
+//return Scaffold(
+//body: Stack(
+//fit: StackFit.passthrough,
+//children: <Widget>[
+//searchBox(),
+//searchButton(),
+//]
+//)
+//);
