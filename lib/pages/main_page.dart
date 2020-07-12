d424bd658ef8edc315619ac9a3925e7152bceb1a
pen <pen@stacksoft.ru>
2020-07-12 17:50:21 +0300

    страница с данными здания и строкой поиска собранная из новых компонентов

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:steriaf/ui_components/components.dart' as ui;
+
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: ListView(children: <Widget>[
+          SizedBox(height: 20.0),
+          ui.SearchBlockWidget(),
+          ui.HouseCardInfoWidget(),
+          ui.VersionInfoWidget(),
+        ])
+      )
+    );
+  }
+}
