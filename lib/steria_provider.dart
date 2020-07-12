9a34fcb6c515850f9c689d70413da4ec1463f967
pen <pen@stacksoft.ru>
2020-07-13 00:22:23 +0300

    saved entered address

--- lib/steria_provider.dart
+++ lib/steria_provider.dart
@@ -1,18 +1,26 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:steriaf/house.dart';
 
 
 class HouseProvider with ChangeNotifier, DiagnosticableTreeMixin {
-  HouseData _houseData;
+  String _currentAddress;
+  String get currentAddress => this._currentAddress;
+
+  HouseData _houseData = HouseData();
   HouseData get houseData => this._houseData;
 
   bool _isFail = false;
   bool get isFail => this._isFail;
 
+  void changeAddress(String address){
+    this._currentAddress = address;
+    notifyListeners();
+  }
+
+
   void loadData(){
+
     this._houseData = HouseData(
         title: 'Test title1',
         architect: 'Architect',
