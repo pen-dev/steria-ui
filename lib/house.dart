d905b21a2f4e191f4488e757d2e68a5ff62bfa53
Vladislav <pelipenkovladislav@gmail.com>
2020-07-23 21:31:51 +0000

    Revert "Merge branch '5-block-architecture' into 'master'"
    This reverts merge request !1

--- lib/house.dart
+++ lib/house.dart
@@ -1,6 +1,6 @@
 import 'dart:convert' as convert;
-
 import 'package:flutter/material.dart';
+
 import 'package:http/http.dart' as http;
 
 class House{
@@ -46,64 +46,4 @@ class House{
     page.setState(() {});
 
   }
-}
-
-class SteriaBaseException implements Exception{
-
-}
-
-class HouseDataLoader {
-
-  String _url = 'https://steria.herokuapp.com/v1/house';
-
-  HouseData _houseData = HouseData();
-
-  String _replaceElseNull(String value){
-    if (value == 'null'){
-      return 'Неизвестно';
-    }
-    
-    return value;
-  }
-
-  void _load(String address) async {
-    var response = await http.get(
-      '$_url?address=$address',
-    );
-    Map<String, dynamic> data = convert.jsonDecode(response.body);
-    print('$data');
-
-    if (response.statusCode == 200) {
-      this._houseData.title = _replaceElseNull(data['r_name'].toString());
-      this._houseData.architect = _replaceElseNull(data['r_architect'].toString());
-      this._houseData.year = _replaceElseNull(data['r_years_string'].toString());
-      this._houseData.style = _replaceElseNull(data['r_style'].toString());
-      this._houseData.fullAddress = _replaceElseNull(data['r_adress'].toString());
-
-    } else {
-      throw SteriaBaseException();
-    }
-  }
-
-  Future<HouseData> load(String address) async {
-    await this._load(address);
-    return this._houseData;
-  }
-}
-
-
-class HouseData{
-  String title;
-  String architect;
-  String fullAddress;
-  String style;
-  String year;
-
-  HouseData({
-    this.title='',
-    this.architect='',
-    this.fullAddress='',
-    this.style='',
-    this.year=''
-});
 }
